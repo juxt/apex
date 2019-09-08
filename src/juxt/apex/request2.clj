0d4eb7c73f248f53da6cb8afbce49f3a6f3fa41b
Malcolm Sparks <mal@juxt.pro>
2019-09-08 07:38:09 +0100

    Add conditional requests

--- src/juxt/apex/request2.clj
+++ src/juxt/apex/request2.clj
@@ -16,23 +16,74 @@
   {:name "Add API"
    :wrap (fn [h api]
            (fn [req respond raise]
-             (log/trace "foo, api is" api)
              (h req respond raise)))})
 
-(defn openapi-handler [doc]
+;; -- BEGIN: Conditional Requests --
 
+(defmulti validator->header (fn [[k v]] k))
+
+(defmethod validator->header :apex/entity-tag
+  [[_ v]] ["ETag" (:value v)])
+
+(defn to-rfc-1123-date-time [instant]
+  (.format java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME
+           (java.time.ZonedDateTime/ofInstant instant (java.time.ZoneId/of "GMT"))))
+
+(defn from-rfc-1123-date-time [s]
+  (some->> s
+           (.parse java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME)
+           (java.time.Instant/from)))
+
+(defmethod validator->header :apex/last-modified
+  [[_ v]]
+  ["Last-Modified" (to-rfc-1123-date-time (:value v))])
+
+(defn since? [instant since]
+  (= (.compareTo instant since) 1))
+
+(defn wrap-conditional-request [h validators]
+  (fn [req respond raise]
+    (let [operation (:oas/operation req)
+          opId (get operation "operationId")]
+      (if validators
+        (validators
+         req
+         (fn [req]
+           (let [since (from-rfc-1123-date-time (get-in req [:headers "if-modified-since"]))
+                 modified (:value (:apex/last-modified req))]
+
+             (if (and since modified (not (since? modified since)))
+               (respond {:status 304})
+               (h req
+                  (fn [response]
+                    (respond (update response :headers merge (into {} (map validator->header (select-keys req [:apex/entity-tag :apex/last-modified]))))))
+                  raise))))
+         raise)
+        ;; No validators
+        (h req respond raise)))))
+
+;; -- END: Conditional Requests --
+
+(defn openapi-handler [doc options]
   (let [routes
         (vec
          (for [[path path-item] (get doc "paths")]
-           [path
-            (apply merge
-                   (for [[method operation] path-item
-                         :let [method (keyword method)]]
-                     {method
-                      {:name (keyword (get operation "operationId"))
-                       :handler (fn [req respond raise] (respond {:status 200 :body "OK"}))
-                       :middleware []
-                       }}))]))
+           (let [resource (get-in options [:apex/resources path])]
+             [path
+              (apply merge
+                     (for [[method operation] path-item
+                           :let [method (keyword method)
+                                 operation-id (get operation "operationId")]]
+                       {method
+                        {:name (keyword operation-id)
+                         :handler (or (get-in resource [:apex/methods method :handler])
+                                      (fn [req respond raise]
+                                        (respond
+                                         {:status 500
+                                          :body (format "Missing operation: %s" operation-id)})))
+                         :middleware [
+                                      [wrap-conditional-request (:apex/validators resource)]
+                                      ]}}))])))
 
         router
         (ring/router
--- test/juxt/apex/request2_test.clj
+++ test/juxt/apex/request2_test.clj
@@ -12,27 +12,146 @@
    [juxt.apex.yaml :as yaml]
    [ring.mock.request :as mock]))
 
+(def pets [{:type "cat"}
+           {:type "dog"}
+           {:type "goat"}
+           {:type "goldfish"}
+           {:type "lizard"}])
+
+#_(let
+    [doc (yaml/parse-string
+          (slurp
+           (io/resource "juxt/apex/openapi-examples/petstore.yaml")))
+     app (openapi-handler
+          doc
+          {:apex/resources
+           {"/pets"
+
+            {:apex/validators
+             (fn [req callback raise]
+               (callback
+                ;; Expectation is to return a new request with
+                ;; validators merged.
+                (merge
+                 req
+                 {:apex/entity-tag
+                  ;; The purpose of this map is also to
+                  ;; indicate strong or weak validator strength.
+                  {:value "123"}
+
+                  :apex/last-modified
+                  {:value (java.time.Instant/parse "2012-12-04T04:21:00Z")}})))
+
+             :apex/methods
+             {:get
+              {:handler
+               (fn [req respond raise]
+                 (respond {:status 200 :body pets}))}}}}})]
+
+  @(call-handler app {:request-method :get :uri "/pets"})
+  )
 
 (deftest app-test
-  (let [doc (yaml/parse-string (slurp (io/resource "juxt/apex/openapi-examples/petstore.yaml")))
-        app (openapi-handler doc)]
+  (let [doc (yaml/parse-string
+             (slurp
+              (io/resource "juxt/apex/openapi-examples/petstore.yaml")))
+        app (openapi-handler
+             doc
+             {:apex/resources
+              {"/pets"
+
+               {:apex/validators
+                (fn [req callback raise]
+                  (callback
+                   ;; Expectation is to return a new request with
+                   ;; validators merged.
+                   (merge
+                    req
+                    {:apex/entity-tag
+                     ;; The purpose of this map is also to
+                     ;; indicate strong or weak validator strength.
+                     {:value "123"}
+
+                     :apex/last-modified
+                     {:value (java.time.Instant/parse "2012-12-04T04:21:00Z")}})))
+
+                :apex/methods
+                {:get
+                 {:handler
+                  (fn [req respond raise]
+                    (respond {:status 200 :body pets}))}}}}})]
 
     (testing "OK"
-      (is (= 200 (:status @(call-handler app {:request-method :get :uri "/pets"})))))
+      (let [{:keys [status body]} @(call-handler app {:request-method :get :uri "/pets"})]
+        (is (= 200 status))
+        (is (= pets body))))
 
     (testing "Not found"
-      (is (= 404 (:status @(call-handler app {:request-method :get :uri "/pets2"})))))
+      (let [{:keys [status]}
+            @(call-handler
+              app
+              {:request-method :get
+               :uri "/pets2"})]
+        (is (= 404 status))))
 
     (testing "No such method"
-      (is (= 405 (:status @(call-handler app {:request-method :delete :uri "/pets"})))))
+      (let [{:keys [status]} @(call-handler
+                               app
+                               {:request-method :delete
+                                :uri "/pets"})]
+        (is (= 405 status))))
 
     (testing "OPTIONS"
-      (let [{:keys [status headers]} @(call-handler app {:request-method :options :uri "/pets"})]
+      (let [{:keys [status headers]}
+            @(call-handler
+              app
+              {:request-method :options
+               :uri "/pets"})]
         (is (= 200 status))
-        (is (= {"Allow" "GET,POST,OPTIONS"} headers))))))
+        (is (= {"Allow" "GET,POST,OPTIONS"} headers))))
 
+    (testing "Conditional requests"
 
-(let [doc (yaml/parse-string (slurp (io/resource "juxt/apex/openapi-examples/petstore.yaml")))
-      app (openapi-handler doc)]
+      (let [{:keys [status headers body]}
+            @(call-handler
+              app
+              {:request-method :get
+               :uri "/pets"})]
 
-  @(call-handler app {:request-method :get :uri "/pets"}))
+        (testing "cache-update"
+
+          (let [last-modified-date (get headers "Last-Modified")]
+
+            (testing "last-modified header returned as expected"
+              (is (= "Tue, 4 Dec 2012 04:21:00 GMT" last-modified-date)))
+
+            (let [{:keys [status headers body]}
+                  @(call-handler app {:request-method :get
+                                      :uri "/pets"
+                                      :headers {"if-modified-since" last-modified-date}})]
+
+              (testing "304 if we use the same date in request"
+                (is (= 304 status))))
+
+            (let [{:keys [status headers body]}
+                  @(call-handler app {:request-method :get
+                                      :uri "/pets"
+                                      :headers {"if-modified-since"
+                                                (juxt.apex.request/to-rfc-1123-date-time
+                                                 (java.time.Instant/parse "2019-01-01T00:00:00Z"))}})]
+
+              (testing "304 if we use a future date in the request"
+                (is (= 304 status))))
+
+            (let [{:keys [status headers body]}
+                  @(call-handler app {:request-method :get
+                                      :uri "/pets"
+                                      :headers {"if-modified-since"
+                                                (juxt.apex.request/to-rfc-1123-date-time
+                                                 (java.time.Instant/parse "2010-01-01T00:00:00Z"))}})]
+
+              (testing "200 if we use a prior date in the request"
+                (is (= 200 status)))
+
+              (testing "last-modified header returned as normal when if-modified-since request header exists"
+                (is (= "Tue, 4 Dec 2012 04:21:00 GMT" (get headers "Last-Modified")))))))))))
