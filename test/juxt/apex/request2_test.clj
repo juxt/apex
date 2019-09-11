3967260442a4aa5146587bb87f69ad29e78ec726
Malcolm Sparks <mal@juxt.pro>
2019-09-11 19:41:51 +0100

    Refactor tests into individual tests
    It's easier to run individual tests from the REPL

--- test/juxt/apex/request2_test.clj
+++ test/juxt/apex/request2_test.clj
@@ -5,12 +5,14 @@
    [clojure.java.io :as io]
    [clojure.string :as str]
    [clojure.tools.logging :as log]
-   [clojure.test :refer [deftest is testing]]
+   [clojure.test :refer [deftest is testing use-fixtures]]
    [jsonista.core :as j]
    [juxt.apex.request2 :refer [openapi-handler]]
-   [juxt.apex.test-util :refer [call-handler]]
    [juxt.apex.yaml :as yaml]
    [ring.mock.request :as mock]))
+
+;; The outer handler, for testing Apex middleware
+(def ^:dynamic *app*)
 
 (def database
   (atom {"1" {"name" "Sven" "type" "Dog"}
@@ -19,7 +21,7 @@
          "4" {"name" "Kaia" "type" "Cat"}
          "5" {"name" "Vega" "type" "Dog"}}))
 
-(defn handler []
+(defn test-handler []
   (let [doc (yaml/parse-string
              (slurp
               (io/resource "juxt/apex/openapi-examples/petstore.yaml")))]
@@ -32,7 +34,8 @@
         {:get
          {:handler
           (fn [req respond raise]
-            (respond {:status 200 :body (vals @database)}))}}
+            (respond {:status 200
+                      :body (vals @database)}))}}
 
         :apex/validators
         (fn [req callback raise]
@@ -61,108 +64,119 @@
                  {:status 200 :body pet}
                  {:status 404}))))}}}}})))
 
-(deftest app-test
-  (let [app (handler)]
+(defn request [request]
+  (assert *app*)
+  (let [p (promise)]
+    (*app*
+     request
+     (fn [response] (deliver p response))
+     (fn [err] (deliver p err)))
+    @p))
 
-    (testing "GET /pets is OK and returns pets"
-      (let [{:keys [status body]}
-            @(call-handler app {:request-method :get :uri "/pets"})]
-        (is (= 200 status))
-        (is (= (vals @database) body))))
+(use-fixtures :once (fn [f] (binding [*app* (test-handler)] (f))))
 
-    (testing "Not found"
-      (let [{:keys [status]}
-            @(call-handler
-              app
-              {:request-method :get
-               :uri "/pets2"})]
-        (is (= 404 status))))
+(deftest happy-path-test
+  (testing "GET /pets is OK and returns pets"
+    (let [{:keys [status body]}
+          (request {:request-method :get :uri "/pets"})]
+      (is (= 200 status))
+      (is (= (vals @database) body))))
+  (testing "Get pet by id is OK"
+    (let [{:keys [status body]}
+          (request {:request-method :get :uri "/pets/2"})]
+      (is (= 200 status))
+      (is (= (get @database "1") {"name" "Sven" "type" "Dog"}))
+      )))
 
-    (testing "Get pet by id is OK"
-      (let [{:keys [status body]}
-            @(call-handler app {:request-method :get :uri "/pets/2"})]
-        (is (= 200 status))
-        (is (= (get @database "1") {"name" "Sven" "type" "Dog"}))))
+(deftest not-found-test
+  (testing "Not found"
+    (let [{:keys [status]}
+          (request
+            {:request-method :get
+             :uri "/pets2"})]
+      (is (= 404 status)))))
 
-    (testing "No such method"
-      (let [{:keys [status]}
-            @(call-handler
-              app
-              {:request-method :delete
-               :uri "/pets"})]
-        (is (= 405 status))))
+;; Reitit is able to return a 405 if the correct URL is chosen but
+;; with the wrong method.
+(deftest no-such-method-test
+  (testing "No such method"
+    (let [{:keys [status]}
+          (request
+           {:request-method :delete
+            :uri "/pets"})]
+      (is (= 405 status)))))
 
-    (testing "OPTIONS"
-      (let [{:keys [status headers]}
-            @(call-handler
-              app
-              {:request-method :options
-               :uri "/pets"})]
-        (is (= 200 status))
-        (is (= {"Allow" "GET,HEAD,POST,OPTIONS"} headers))))
+(deftest options-test
+  (testing "OPTIONS"
+    (let [{:keys [status headers]}
+          (request
+           {:request-method :options
+            :uri "/pets"})]
+      (is (= 200 status))
+      (is (= {"Allow" "GET,HEAD,POST,OPTIONS"} headers)))))
 
-    (testing "HEAD"
-      (let [head-response
-            @(call-handler
-              app
-              {:request-method :head
-               :uri "/pets"})
-            get-response
-            @(call-handler
-              app
-              {:request-method :get
-               :uri "/pets"})]
-        (testing "HEAD is successful"
-          (is (= 200 (:status head-response))))
-        (testing "GET and HEAD have identical headers"
-          (is (= (:headers get-response) (:headers head-response))))
-        (testing "Body of HEAD is nil"
-          (is (nil? (:body head-response)))
-          ;; And not just because the GET body is nil
-          (is (not (nil? (:body get-response)))))))
+(deftest head-test
+  (testing "HEAD"
+    (let [head-response
+          (request
+           {:request-method :head
+            :uri "/pets"})
+          get-response
+          (request
+           {:request-method :get
+            :uri "/pets"})]
+      (testing "HEAD is successful"
+        (is (= 200 (:status head-response))))
+      (testing "GET and HEAD have identical headers"
+        (is (= (:headers get-response) (:headers head-response))))
+      (testing "Body of HEAD is nil"
+        (is (nil? (:body head-response)))
+        ;; And not just because the GET body is nil
+        (is (not (nil? (:body get-response))))))))
 
-    (testing "Conditional requests"
+;; Tests conditional requests
+(deftest not-modified-test
+  (testing "Conditional requests"
 
-      (let [{:keys [status headers body]}
-            @(call-handler
-              app
-              {:request-method :get
-               :uri "/pets"})]
+    (let [{:keys [status headers body]}
+          (request
+           {:request-method :get
+            :uri "/pets"})]
 
-        (testing "cache-update"
+      (testing "cache-update"
 
-          (let [last-modified-date (get headers "Last-Modified")]
+        (let [last-modified-date (get headers "Last-Modified")]
 
-            (testing "last-modified header returned as expected"
-              (is (= "Tue, 4 Dec 2012 04:21:00 GMT" last-modified-date)))
+          (testing "last-modified header returned as expected"
+            (is (= "Tue, 4 Dec 2012 04:21:00 GMT" last-modified-date)))
 
-            (let [{:keys [status headers body]}
-                  @(call-handler app {:request-method :get
-                                      :uri "/pets"
-                                      :headers {"if-modified-since" last-modified-date}})]
+          (let [{:keys [status headers body]}
+                (request {:request-method :get
+                          :uri "/pets"
+                          :headers {"if-modified-since" last-modified-date}})]
 
-              (testing "304 if we use the same date in request"
-                (is (= 304 status))))
+            (testing "304 if we use the same date in request"
+              (is (= 304 status))))
 
-            (let [{:keys [status headers body]}
-                  @(call-handler app {:request-method :get
-                                      :uri "/pets"
-                                      :headers {"if-modified-since"
-                                                (juxt.apex.request/to-rfc-1123-date-time
-                                                 (java.time.Instant/parse "2019-01-01T00:00:00Z"))}})]
+          (let [{:keys [status headers body]}
+                (request {:request-method :get
+                          :uri "/pets"
+                          :headers {"if-modified-since"
+                                    (juxt.apex.request/to-rfc-1123-date-time
+                                     (java.time.Instant/parse "2019-01-01T00:00:00Z"))}})]
 
-              (testing "304 if we use a future date in the request"
-                (is (= 304 status))))
+            (testing "304 if we use a future date in the request"
+              (is (= 304 status))))
 
-            (let [{:keys [status headers body]}
-                  @(call-handler app {:request-method :get
-                                      :uri "/pets"
-                                      :headers {"if-modified-since"
-                                                (juxt.apex.request/to-rfc-1123-date-time
-                                                 (java.time.Instant/parse "2010-01-01T00:00:00Z"))}})]
+          (let [{:keys [status headers body]}
+                (request {:request-method :get
+                          :uri "/pets"
+                          :headers {"if-modified-since"
+                                    (juxt.apex.request/to-rfc-1123-date-time
+                                     (java.time.Instant/parse "2010-01-01T00:00:00Z"))}})]
 
-              (testing "200 if we use a prior date in the request"
-                (is (= 200 status)))
+            (testing "200 if we use a prior date in the request"
+              (is (= 200 status)))
 
-              (testing "last-modified header returned as normal when if-modified-since request header exists"
-                (is (= "Tue, 4 Dec 2012 04:21:00 GMT" (get headers "Last-Modified")))))))))))
+            (testing "last-modified header returned as normal when if-modified-since request header exists"
+              (is (= "Tue, 4 Dec 2012 04:21:00 GMT" (get headers "Last-Modified"))))))))))
