7f7551bd084678b2052ca893dc412807616c3451
Malcolm Sparks <mal@juxt.pro>
2019-10-16 13:43:32 +0100

    Add ref resolution utility function

--- src/juxt/apex/alpha2/util.clj
+++ src/juxt/apex/alpha2/util.clj
@@ -1,6 +1,8 @@
 ;; Copyright © 2019, JUXT LTD.
 
-(ns juxt.apex.alpha2.util)
+(ns juxt.apex.alpha2.util
+  (:require
+   [juxt.jinx-alpha.resolve :refer [resolve-ref]]))
 
 (defn to-rfc-1123-date-time [instant]
   (.format java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME
@@ -10,3 +12,11 @@
   (some->> s
            (.parse java.time.format.DateTimeFormatter/RFC_1123_DATE_TIME)
            (java.time.Instant/from)))
+
+(defn resolve-json-ref
+  "Resolve a json reference. If ref is already resolved, then return it
+  unchanged."
+  [ref {:keys [base-document]}]
+  (if (contains? ref "$ref")
+    (resolve-ref ref base-document {})
+    [ref nil]))
