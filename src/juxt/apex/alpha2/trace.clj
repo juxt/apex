224d2890dbecfeb57f6e381eaacbe8f683dbf188
Tom Taylor <tmt@juxt.pro>
2019-10-17 09:21:11 +0100

    Add rjsf generation

--- src/juxt/apex/alpha2/trace.clj
+++ src/juxt/apex/alpha2/trace.clj
@@ -6,6 +6,7 @@
    [clojure.string :as str]
    [clojure.pprint :refer [pprint]]
    [ring.util.codec :as codec]
+   [jsonista.core :as json]
    [juxt.apex.alpha2.html :as html]
    [juxt.apex.alpha2.util :refer [resolve-json-ref]]))
 
@@ -53,7 +54,7 @@
       "creation-form"
       (delay
         (let [doc (get-in req [:reitit.core/match :data :post :apex/openapi])]
-          (pr-str
+          (json/write-value-as-string
            (some->
             (get-in req [:reitit.core/match :data :post :apex/operation "requestBody" "content" "application/json" "schema"])
             (resolve-json-ref {:base-document doc})
