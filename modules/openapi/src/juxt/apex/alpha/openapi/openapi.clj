911643b50984a1041c0d208d2448566bc352ea8c
Malcolm Sparks <mal@juxt.pro>
2019-12-11 10:39:28 +0000

    refactoing

--- modules/openapi/src/juxt/apex/alpha/openapi/openapi.clj
+++ modules/openapi/src/juxt/apex/alpha/openapi/openapi.clj
@@ -22,19 +22,79 @@
      [condreq/wrap-conditional-request (:apex/validators resource)]
      ]}})
 
-(defn openapi->reitit-routes
-  "Create a sequence of Reitit route/resource pairs from a given OpenAPI
-  document"
-  [doc
+(defn openapi-path-item->route
+  [path
+   path-item
    {:apex/keys
-    [resources ; resource implementations mapped by path, see docs
-     add-implicit-head? ; whether HEAD should be supported implicitly
+    [resources     ; resource implementations mapped by path, see docs
+     add-implicit-head?  ; whether HEAD should be supported implicitly
      middleware
-     ]
+     openapi]
     :or {resources {}
          add-implicit-head? true
          middleware []}
     :as options}]
+  (let [resource (get resources path)]
+    (apply
+     merge
+     ;; Support default HEAD method
+     (when add-implicit-head? (implicit-head-method resource))
+
+     (for [[method {operation-id "operationId"
+                    :as operation}]
+           path-item
+           :let [method (keyword method)]]
+       {method
+        {:name (keyword operation-id)
+
+         :handler
+         (let [default-response
+               {:status 500
+                ;; TODO: Create enough data for this to
+                ;; be fancified into HTML by later
+                ;; middleware.
+                :body (format "Missing method handler for OpenAPI operation %s at path %s\n" operation-id path)}]
+           (or
+
+            (get-in resource [:apex/methods method :handler])
+
+            (fn
+              ([req] default-response)
+              ([req respond raise]
+               (respond default-response)))))
+
+         :middleware middleware
+
+         ;; Middleware to be passed in as a parameter
+         ;;:middleware
+         #_[
+            ;; TODO: For consistency, copy these naming Reitit
+            ;; conventions in all Apex middleware.
+            ;;[exception/exception-middleware]
+
+            [condreq/wrap-conditional-request (:apex/validators resource)]
+            params/wrap-coerce-parameters
+            request-body/wrap-process-request-body
+            session-mw
+            ]
+
+         ;; This is needed by some middleware to know whether to
+         ;; mount, e.g. a GET method body "has no defined
+         ;; semantics".
+         :apex/method method
+
+         :apex/operation operation
+         :apex/operation-id operation-id
+         :apex/path path
+
+         ;; This is needed by some functions as the base document
+         ;; in $ref resolution
+         :apex/openapi (with-meta openapi {:apex.trace/hide true})}}))))
+
+(defn openapi->reitit-routes
+  "Create a sequence of Reitit route/resource pairs from a given OpenAPI
+  document"
+  [doc opts]
   (let [#_[session-mw
            [session/session-middleware
             {:store (ring.middleware.session.cookie/cookie-store)
@@ -42,71 +102,24 @@
     (into
      []
      (for [[path path-item] (get doc "paths")]
-       (let [resource (get resources path)]
-         [path
-          (apply
-           merge
-           ;; Support default HEAD method
-           (when add-implicit-head? (implicit-head-method resource))
+       [path (openapi-path-item->route
+              path
+              path-item
+              ;; Needed for $ref resolution, but ideally $ref
+              ;; resolution should happen beforehand such that the
+              ;; called function doesn't need this information.
+              (assoc opts :apex/openapi doc))]))))
 
-           (for [[method {operation-id "operationId"
-                          :as operation}]
-                 path-item
-                 :let [method (keyword method)]]
-             {method
-              {:name (keyword operation-id)
-
-               :handler
-               (let [default-response
-                     {:status 500
-                      ;; TODO: Create enough data for this to
-                      ;; be fancified into HTML by later
-                      ;; middleware.
-                      :body (format "Missing method handler for OpenAPI operation %s at path %s\n" operation-id path)}]
-                 (or
-
-                  (get-in resource [:apex/methods method :handler])
-
-                  (fn
-                    ([req] default-response)
-                    ([req respond raise]
-                     (respond default-response)))))
-
-               :middleware middleware
-
-               ;; Middleware to be passed in as a parameter
-               ;;:middleware
-               #_[
-                  ;; TODO: For consistency, copy these naming Reitit
-                  ;; conventions in all Apex middleware.
-                  ;;[exception/exception-middleware]
-
-                  [condreq/wrap-conditional-request (:apex/validators resource)]
-                  params/wrap-coerce-parameters
-                  request-body/wrap-process-request-body
-                  session-mw
-                  ]
-
-               ;; This is needed by some middleware to know whether to
-               ;; mount, e.g. a GET method body "has no defined
-               ;; semantics".
-               :apex/method method
-
-               :apex/operation operation
-               :apex/operation-id operation-id
-               :apex/path path
-
-               ;; This is needed by some functions as the base document
-               ;; in $ref resolution
-               :apex/openapi (with-meta doc {:apex.trace/hide true})}}))])))))
-
+;; @mal -> @dmc: based on our discussion about Apex being a 'good citizen' in
+;; the Ring ecosystem, this is the function which begins to deviate
+;; from this goal - it's arguably where the complexity begins.
 (defn create-api-router [doc path {:apex/keys [request-history-atom] :as opts}]
   (ring/router
    (openapi->reitit-routes doc opts)
    (merge
     {:path path}
     #_(when request-history-atom
-      {:reitit.middleware/transform (trace/trace-middleware-transform request-history-atom)}))))
+        {:reitit.middleware/transform (trace/trace-middleware-transform request-history-atom)}))))
 
 ;; TODO: Rename from create-api-route to something more intuitive like create-api
 (defn create-api-route
