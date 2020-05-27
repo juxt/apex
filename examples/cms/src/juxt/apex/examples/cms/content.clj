a55e848ac4ae5fad813cd4e4d33ff33b7e0c3b7b
Malcolm Sparks <mal@juxt.pro>
2020-05-27 10:28:35 +0100

    checkpoint

--- examples/cms/src/juxt/apex/examples/cms/content.clj
+++ examples/cms/src/juxt/apex/examples/cms/content.clj
@@ -47,73 +47,77 @@
 
 (defn content-txes []
   (map compute-etag
-       (concat
+       (remove nil?
+               (concat
 
-        ;; Content
-        (->>
-         (map
-          (comp compute-content-length ingest-content)
-          (edn/read-string
-           {:readers {'crux/uri (fn [x] (java.net.URI. x))
-                      'crux.cms/file (fn [path]
-                                       (.getAbsolutePath (io/file WEBSITE_REPO_DIR path)))}}
-           (slurp "src/juxt/apex/examples/cms/content.edn"))))
+                ;; Content
+                (->>
+                 (map
+                  (comp compute-content-length ingest-content)
+                  (edn/read-string
+                   {:readers {'crux/uri (fn [x] (java.net.URI. x))
+                              'crux.cms/file (fn [path]
+                                               (.getAbsolutePath (io/file WEBSITE_REPO_DIR path)))}}
+                   (slurp "src/juxt/apex/examples/cms/content.edn"))))
 
-        ;; Selmer template sources
-        (let [dir (io/file WEBSITE_REPO_DIR "juxt.website/resources/templates")]
-          (for [f (file-seq dir)
-                :when (.isFile f)
-                :let [p (str (.relativize (.toPath dir) (.toPath f)))]]
-            (merge
-             {:crux.db/id (java.net.URI. (str "https://juxt.pro/_sources/templates/" p))
-              :crux.web/content-type "text/plain;charset=utf-8"
-              :crux.web/content-language "en"}
-             (slurp-file-as-string f))))
+                ;; Selmer template sources
+                (let [dir (io/file WEBSITE_REPO_DIR "juxt.website/resources/templates")]
+                  (for [f (file-seq dir)
+                        :when (.isFile f)
+                        :let [p (str (.relativize (.toPath dir) (.toPath f)))]]
+                    (merge
+                     {:crux.db/id (java.net.URI. (str "https://juxt.pro/_sources/templates/" p))
+                      :crux.web/content-type "text/plain;charset=utf-8"
+                      :crux.web/content-language "en"}
+                     (slurp-file-as-string f))))
 
-        ;; Plan repo site sources
-        (let [dir (io/file PLAN_REPO_DIR "site")]
-          (for [f (file-seq dir)
-                :when (.isFile f)
-                :let [p (str (.relativize (.toPath dir) (.toPath f)))]
-                :when (not (.startsWith p "."))]
-            (condp re-matches p
-              #".*\.adoc"
-              (merge
-               {:crux.db/id (java.net.URI. (str "https://juxt.pro/_sources/plan/site/" p))
-                :crux.web/content-type "text/plain;charset=utf-8"
-                :crux.web/content-language "en"}
-               (slurp-file-as-string f))
+                ;; Plan repo site sources
+                (let [dir (io/file PLAN_REPO_DIR "site")]
+                  (for [f (file-seq dir)
+                        :when (.isFile f)
+                        :let [p (str (.relativize (.toPath dir) (.toPath f)))]
+                        :when (not (.startsWith p "."))]
+                    (condp re-matches p
+                      #".*\.adoc"
+                      (merge
+                       {:crux.db/id (java.net.URI. (str "https://juxt.pro/_sources/plan/site/" p))
+                        :crux.web/content-type "text/plain;charset=utf-8"
+                        :crux.web/content-language "en"}
+                       (slurp-file-as-string f))
 
-              #".*\.svg"
-              (merge
-               {:crux.db/id (java.net.URI. (str "https://juxt.pro/" p))
-                :crux.web/content-type "image/svg+xml"
-                :crux.web/content-language "en"
-                :crux.ac/classification :public}
-               (slurp-file-as-string f))
+                      #".*\.svg"
+                      (merge
+                       {:crux.db/id (java.net.URI. (str "https://juxt.pro/" p))
+                        :crux.web/content-type "image/svg+xml"
+                        :crux.web/content-language "en"
+                        :crux.ac/classification :public}
+                       (slurp-file-as-string f))
 
-              #".*\.png"
-              (merge
-               {:crux.db/id (java.net.URI. (str "https://juxt.pro/" p))
-                :crux.web/content-type "image/png"
-                :crux.ac/classification :public}
-               (slurp-file-as-b64encoded-string f)))))
+                      #".*\.png"
+                      (merge
+                       {:crux.db/id (java.net.URI. (str "https://juxt.pro/" p))
+                        :crux.web/content-type "image/png"
+                        :crux.ac/classification :public}
+                       (slurp-file-as-b64encoded-string f))
 
-        ;; TODO: This should be the place where dependencies are detected
-        ;; and placed into the graph along with the document as
-        ;; references.
+                      #"Makefile" nil
+                      )))
 
-        ;; Sass
-        #_(let [dir (io/file WEBSITE_REPO_DIR "juxt.website")]
-            (for [[dir path]
-                  [[(io/file dir "src") "frontpage.scss"]
-                   [dir "assets/css/variables.scss"]
-                   ]]
-              (let [f (io/file dir path)]
+                ;; TODO: This should be the place where dependencies are detected
+                ;; and placed into the graph along with the document as
+                ;; references.
 
-                {:crux.db/id (java.net.URI. (str "https://juxt.pro/_sources/sass/" path))
-                 :crux.web/content-type "text/plain;charset=utf-8"
-                 :crux.web/content-language "en"
-                 :crux.web/last-modified (java.util.Date. (.lastModified f))
-                 :crux.web/content (slurp f)
-                 :crux.ac/classification :public}))))))
+                ;; Sass
+                #_(let [dir (io/file WEBSITE_REPO_DIR "juxt.website")]
+                    (for [[dir path]
+                          [[(io/file dir "src") "frontpage.scss"]
+                           [dir "assets/css/variables.scss"]
+                           ]]
+                      (let [f (io/file dir path)]
+
+                        {:crux.db/id (java.net.URI. (str "https://juxt.pro/_sources/sass/" path))
+                         :crux.web/content-type "text/plain;charset=utf-8"
+                         :crux.web/content-language "en"
+                         :crux.web/last-modified (java.util.Date. (.lastModified f))
+                         :crux.web/content (slurp f)
+                         :crux.ac/classification :public})))))))
