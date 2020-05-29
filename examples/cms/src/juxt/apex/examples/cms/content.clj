93c43c2464e6e7cd513bd826b3c7e90e4588f1ae
Malcolm Sparks <mal@juxt.pro>
2020-05-30 00:57:25 +0100

    checkpoint

--- examples/cms/src/juxt/apex/examples/cms/content.clj
+++ examples/cms/src/juxt/apex/examples/cms/content.clj
@@ -4,7 +4,6 @@
    [clojure.java.io :as io]))
 
 (def WEBSITE_REPO_DIR (io/file (System/getProperty "user.home") "src/github.com/juxt/website"))
-(def PLAN_REPO_DIR (io/file (System/getProperty "user.home") "src/github.com/juxt/plan"))
 
 (defn slurp-file-as-b64encoded-string [f]
   (try
@@ -89,55 +88,4 @@
                      {:crux.db/id (java.net.URI. (str "https://juxt.pro/_sources/templates/" p))
                       :apex/content-type "text/plain;charset=utf-8"
                       :apex/content-language "en"}
-                     (slurp-file-as-string f))))
-
-                ;; Plan repo site sources
-                #_(let [dir (io/file PLAN_REPO_DIR "site")]
-                  (for [f (file-seq dir)
-                        :when (.isFile f)
-                        :let [p (str (.relativize (.toPath dir) (.toPath f)))]
-                        :when (not (.startsWith p "."))]
-                    (condp re-matches p
-                      #".*\.adoc"
-                      (merge
-                       {:crux.db/id (java.net.URI. (str "https://juxt.pro/_sources/plan/site/" p))
-                        :apex/content-type "text/plain;charset=utf-8"
-                        :apex/content-language "en"}
-                       (slurp-file-as-string f))
-
-                      #".*\.svg"
-                      (merge
-                       {:crux.db/id (java.net.URI. (str "https://juxt.pro/" p))
-                        :apex/content-type "image/svg+xml"
-                        :apex/content-language "en"
-                        :apex/classification :public}
-                       (slurp-file-as-string f))
-
-                      #".*\.png"
-                      (merge
-                       {:crux.db/id (java.net.URI. (str "https://juxt.pro/" p))
-                        :apex/content-type "image/png"
-                        :apex/classification :public}
-                       (slurp-file-as-b64encoded-string f))
-
-                      #"Makefile" nil
-                      )))
-
-                ;; TODO: This should be the place where dependencies are detected
-                ;; and placed into the graph along with the document as
-                ;; references.
-
-                ;; Sass
-                #_(let [dir (io/file WEBSITE_REPO_DIR "juxt.website")]
-                    (for [[dir path]
-                          [[(io/file dir "src") "frontpage.scss"]
-                           [dir "assets/css/variables.scss"]
-                           ]]
-                      (let [f (io/file dir path)]
-
-                        {:crux.db/id (java.net.URI. (str "https://juxt.pro/_sources/sass/" path))
-                         :apex/content-type "text/plain;charset=utf-8"
-                         :apex/content-language "en"
-                         :apex/last-modified (java.util.Date. (.lastModified f))
-                         :apex/content (slurp f)
-                         :apex/classification :public})))))))
+                     (slurp-file-as-string f))))))))
