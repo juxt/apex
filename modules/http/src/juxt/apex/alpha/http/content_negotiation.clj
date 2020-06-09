0d33ac6e4a96941a16640c672c3b3e48cc6f8957
Malcolm Sparks <mal@juxt.pro>
2020-06-09 14:19:01 +0100

    checkpoint

--- modules/http/src/juxt/apex/alpha/http/content_negotiation.clj
+++ modules/http/src/juxt/apex/alpha/http/content_negotiation.clj
@@ -4,7 +4,7 @@
   (:require
    [juxt.reap.alpha.api :as reap]))
 
-(defn- match?
+(defn- content-type-match?
   "Return truthy if the given accept-field (reap format) accepts the given
   content-type (reap format). The return value is a the precedence value (if
   matched), nil otherwise."
@@ -41,14 +41,14 @@
      (= "*" (:subtype parsed-accept-field)))
     1))
 
-(defn- select-better-match
+(defn- select-better-content-type-match
   "Designed to be used as a reducing function, if the parsed-accept-field is a
   higher precedence (or same precedence with higher qvalue), return an updated
   best-match map."
 
   [best-match parsed-accept-field]
 
-  (let [precedence (match? parsed-accept-field (:content-type best-match))
+  (let [precedence (content-type-match? parsed-accept-field (:content-type best-match))
         qvalue (get parsed-accept-field :qvalue 1.0)]
 
     (cond-> best-match
@@ -63,11 +63,11 @@
        [:precedence precedence]
        [:apex.debug/parsed-accept-field parsed-accept-field]))))
 
-(defn acceptable-media-type-rating
+(defn acceptable-content-type-rating
   "Determine the given content-type's rating (precedence, qvalue) with respect to
-  what is acceptable. The accepts parameter is a data structure returned from
-  parsing the Accept header with reap. The variant is a map corresponding to the
-  resource of the variant.
+  what is acceptable. The parsed-accept-fields parameter is a data structure
+  returned from parsing the Accept header with reap. The variant is a map
+  corresponding to the resource of the variant.
 
   This function determines the qvalue according to rules of precedence in RFC
   7231 Section 5.3.2 which are independent of the actual content negotation
@@ -81,42 +81,75 @@
   [parsed-accept-fields parsed-content-type]
 
   (reduce
-   select-better-match
+   select-better-content-type-match
    {:qvalue 0.0
     :content-type parsed-content-type}
    parsed-accept-fields))
 
-(defn debug-acceptable-media-type-rating
-  "Same as acceptable-media-type-rating but with reductions, for debugging."
+(defn debug-acceptable-content-type-rating
+  "Same as acceptable-content-type-rating but with reductions, for debugging."
   [parsed-accept-fields parsed-content-type]
   (reductions
-   select-better-match
+   select-better-content-type-match
    {:qvalue 0.0
     :content-type parsed-content-type}
    parsed-accept-fields))
 
-(defn select-max-by
-  "Return the items in the collection that share the maximum numeric value
-  returned by calling the given 1-arity keyfn function with the item as the
-  argument. This is commonly used when negotiating representations based on
-  process-of-elimination techniques."
-  [keyfn coll]
-  (:items
-   (reduce
-    (fn [acc item]
-      (let [val (or (keyfn item) 0)
-            current-max (:val acc)]
-        (cond
-          (> val current-max)
-          (assoc acc
-                 :items [item]
-                 :val val)
+(defn language-match?
+  "Basic filtering as per RFC 4647 Section 3.3.1."
+  [^String language-range ^String language-tag]
 
-          (= val current-max)
-          (update acc :items conj item)
+  (assert (string? language-range))
+  (assert (string? language-tag))
 
-          :else acc)))
-    {:items [] :val 0} coll)))
+  (or
+   ;; "A language range matches a particular language tag if, in a
+   ;; case-insensitive comparison, it exactly equals the tag, …"
+   (.equalsIgnoreCase language-range language-tag)
+
+   ;; "or if it exactly equals a prefix of the tag such that the first character
+   ;; following the prefix is '-'. "
+   (.equalsIgnoreCase
+    (str language-range "-")
+    (subs language-tag 0 (inc (. language-range length))))
+
+   ;; "The special range '*' in a language priority list matches any tag."
+   (.equals language-range "*")))
+
+(defn- select-better-language-match
+  [best-match parsed-accept-language-field]
+  (let [qvalue (get parsed-accept-language-field :qvalue 1.0)]
+    (cond-> best-match
+      (and (language-match?
+            (:language-range parsed-accept-language-field)
+            (:langtag best-match))
+           (> qvalue (get best-match :qvalue 0.0)))
+      (conj
+       [:qvalue qvalue]
+       [:apex.debug/parsed-accept-language-field parsed-accept-language-field]))))
+
+(defn acceptable-language-rating
+  "Determine the given language's rating (precedence, qvalue) with respect to what
+  is acceptable. The parsed-accept-language-fields parameter is a data structure
+  returned from parsing the Accept-Language header with reap. The variant is a
+  map corresponding to the resource of the variant.
+
+  This function determines the qvalue according to rules of precedence in RFC
+  7231 Section 5.3.2 which are independent of the actual content negotation
+  used.
+
+  The qvalue is set to 0 if 'not acceptable' (See RFC 7231 Section 5.3.1). This
+  still gives the content negotiation algorithm the chance of returning a
+  variant, if there are no more preferable variants and if returning one is
+  preferable to returning a 406 status code."
+
+  [parsed-accept-language-fields parsed-language-tag]
+
+  (reduce
+   select-better-language-match
+   {:qvalue 0.0
+    :language-tag parsed-language-tag}
+   parsed-accept-language-fields))
 
 ;; Apache httpd Negotiation Algorithm -- http://httpd.apache.org/docs/current/en/content-negotiation.html#algorithm
 
@@ -143,28 +176,54 @@
 ;;     The algorithm has now selected one 'best' variant, so return it as the response. The HTTP response header Vary is set to indicate the dimensions of negotiation (browsers and caches can use this information when caching the resource). End.
 ;;     To get here means no variant was selected (because none are acceptable to the browser). Return a 406 status (meaning "No acceptable representation") with a response body consisting of an HTML document listing the available variants. Also set the HTTP Vary header to indicate the dimensions of variance.
 
-(defn assign-media-type-quality [parsed-accept-fields]
+(defn assign-content-type-quality [parsed-accept-fields]
   (keep
    (fn [variant]
      (let [qvalue
-           (:qvalue
-            (acceptable-media-type-rating
-             parsed-accept-fields
-             (reap/content-type (:apex.http/content-type variant))))]
+           (when-let [content-type (:apex.http/content-type variant)]
+             (:qvalue
+              (acceptable-content-type-rating
+               parsed-accept-fields
+               (reap/content-type content-type))))]
        (cond-> variant
-         qvalue (conj [:apex.http.content-negotiation/media-type-qvalue qvalue]))))))
+         qvalue (conj [:apex.http.content-negotiation/content-type-qvalue qvalue]))))))
+
+(defn assign-language-quality [parsed-accept-language-fields]
+  (keep
+   (fn [variant]
+     (let [qvalue
+           (when-let [content-language (:apex.http/content-language variant)]
+             (:qvalue
+              (acceptable-language-rating
+               parsed-accept-language-fields
+               (reap/content-language content-language))))]
+       (cond-> variant
+         qvalue (conj [:apex.http.content-negotiation/language-qvalue qvalue]))))))
 
 (defn rate-variants [request variants]
   (sequence
 
-   (assign-media-type-quality
-    (reap/accept
-     (get-in
-      request
-      [:headers "accept"]
-      ;; "A request without any Accept header field implies that the user
-      ;; agent will accept any media type in response". -- test for this
-      "*/*")))
+   (comp
+
+    (assign-content-type-quality
+     (reap/accept
+      (get-in
+       request
+       [:headers "accept"]
+       ;; "A request without any Accept header field implies that the user
+       ;; agent will accept any media type in response". -- test for this
+       ;; -- RFC 7231 Section 5.3.2
+       "*/*")))
+
+    (assign-language-quality
+     (reap/accept-language
+      (get-in
+       request
+       [:headers "accept-language"]
+       ;; "A request without any Accept-Language header field implies that the
+       ;; user agent will accept any language in response."
+       ;; -- RFC 7231 Section 5.3.5
+       "*"))))
 
    ;; TODO: Repeat for other dimensions. Short circuit, so if 0 variants left,
    ;; don't keep parsing headers! But with one left, keep parsing because maybe
@@ -175,6 +234,29 @@
    ;; (with translation software, for example). -- RFC 7231 Section 5.3.5"
 
    variants))
+
+(defn select-max-by
+  "Return the items in the collection that share the maximum numeric value
+  returned by calling the given 1-arity keyfn function with the item as the
+  argument. This is commonly used when negotiating representations based on
+  process-of-elimination techniques."
+  [keyfn coll]
+  (:items
+   (reduce
+    (fn [acc item]
+      (let [val (or (keyfn item) 0)
+            current-max (:val acc)]
+        (cond
+          (> val current-max)
+          (assoc acc
+                 :items [item]
+                 :val val)
+
+          (= val current-max)
+          (update acc :items conj item)
+
+          :else acc)))
+    {:items [] :val 0} coll)))
 
 (defn select-most-acceptable-representation
 
@@ -201,7 +283,7 @@
         ;; the variants with the highest value."
         (select-max-by
          (fn [variant]
-           (* (get variant :apex.http.content-negotiation/media-type-qvalue)
+           (* (get variant :apex.http.content-negotiation/content-type-qvalue)
               (get variant :apex.http/quality-of-source 1.0)))
          variants))
 
@@ -215,3 +297,6 @@
 
       ;; Select the first variant of those remaining
       first])))
+
+;; TODO: Produce an 'explain' for each content negotiation that can be
+;; logged/response on a 406 (and to support debugging)
--- modules/http/test/juxt/apex/http/content_negotiation_test.clj
+++ modules/http/test/juxt/apex/http/content_negotiation_test.clj
@@ -4,17 +4,33 @@
   (:require
    [clojure.test :refer [deftest is are]]
    [juxt.apex.alpha.http.content-negotiation
-    :refer [acceptable-media-type-rating select-most-acceptable-representation]]
+    :refer [acceptable-content-type-rating
+            acceptable-language-rating
+            language-match?
+            select-most-acceptable-representation]]
    [juxt.reap.alpha.api :as reap]
    [ring.mock.request :refer [request]]
    [juxt.reap.alpha.rfc7231 :as rfc7231]
    [juxt.reap.alpha.regex :as re]))
 
+;; TODO: test for content-type-match?
+
+(deftest acceptable-content-type-rating-test
+  (are [content-type expected]
+      (= (select-keys
+          (acceptable-content-type-rating
+           (reap/accept "text/html;q=0.1,text/html;level=2;q=0.4,text/html;level=3;q=0.5")
+           (reap/content-type content-type))
+          [:qvalue :precedence])
+         expected)
+    "text/html;charset=utf-8" {:precedence 3 :qvalue 0.1}
+    "text/html;level=2;charset=utf-8" {:precedence 4 :qvalue 0.4}))
+
 ;; This test represents the table in RFC 7231 Section 5.3.2, where quality
 ;; values are determined from matching a variant's content-type according to
 ;; rules of precedence. These rules are specified in the RFC and are independent
 ;; of the actual content negotiation algorithm that is used.
-(deftest acceptable-media-type-test
+(deftest acceptable-content-type-qvalue-test
 
   (let [accepts
         (reap/accept "text/*;q=0.3, text/html;q=0.7, text/html;level=1, text/html;level=2;q=0.4, */*;q=0.5")]
@@ -22,7 +38,7 @@
     (are [content-type expected]
         (= expected
            (:qvalue
-            (acceptable-media-type-rating
+            (acceptable-content-type-rating
              accepts
              (reap/content-type content-type))))
 
@@ -33,16 +49,41 @@
         "text/html;level=2" 0.4
         "text/html;level=3" 0.7)))
 
-(deftest parameter-matching
-  (are [content-type expected]
+#_(deftest acceptable-language-rating-test
+  (are [language expected]
       (= (select-keys
-          (acceptable-media-type-rating
-           (reap/accept "text/html;q=0.1,text/html;level=2;q=0.4,text/html;level=3;q=0.5")
-           (reap/content-type content-type))
+
           [:qvalue :precedence])
          expected)
     "text/html;charset=utf-8" {:precedence 3 :qvalue 0.1}
     "text/html;level=2;charset=utf-8" {:precedence 4 :qvalue 0.4}))
+
+#_(acceptable-language-rating
+ (reap/accept-language "en")
+ (reap/content-language "en"))
+
+;; This test represents the example in RFC 4647 Section 3.3.1.
+(deftest language-match-test
+  (is
+   (language-match?
+    (:language-range (first (reap/accept-language "en")))
+    (:langtag (first (reap/content-language "en")))))
+
+  (is
+   (language-match?
+    (:language-range (first (reap/accept-language "de-de")))
+    (:langtag (first (reap/content-language "de-DE-1996")))))
+
+  (is
+   (not
+    (language-match?
+     (:language-range (first (reap/accept-language "de-de")))
+     (:langtag (first (reap/content-language "de-Latn-DE"))))))
+
+  (is
+   (language-match?
+    (:language-range (first (reap/accept-language "*")))
+    (:langtag (first (reap/content-language "de"))))))
 
 (deftest select-most-acceptable-representation-test
 
